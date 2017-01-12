41dd0c20eed51142affdc7dd07fe3f47c7f551c9
cache-tlb <liubin1991@outlook.com>
2017-01-12 19:39:17 +0800

    add camera space normal/position

--- Shaders/gbuffer.fs.glsl
+++ Shaders/gbuffer.fs.glsl
@@ -1,13 +1,18 @@
 #version 330
 
-// mode: 1 -> pos
-//       2 -> normal
-//       3 -> depth
-//       4 -> constant value (for debug)
+// mode: 
+//       0 -> constant value (for debug)
+//       1 -> pos (world)
+//       2 -> normal (world)
+//       3 -> depth (camera)
+//       4 -> pos (camera)
+//       5 -> normal (camera)
 uniform int mode;
 
-in vec3 frag_pos;
-in vec3 N;
+in vec3 frag_pos_world;      // frag_pos is the position of a pixel in world coordinate.
+in vec3 frag_pos_cam;       // position in view space;
+in vec3 frag_normal_world;
+in vec3 frag_normal_cam;
 in float depth;
 
 out vec4 outputF;
@@ -15,19 +20,22 @@ out vec4 outputF;
 void main() 
 {
     vec4 ret;
-    if (mode == 1) {
-        // pos
-        vec3 pos = frag_pos;
+    if (mode == 0) {
+        ret = vec4(1.0,0.5,0.5,1.0);
+    } else if (mode == 1) {
+        vec3 pos = frag_pos_world;
         ret = vec4(pos, 1.0);
     } else if (mode == 2) {
-        // normal
-        vec3 normal = normalize(N);
+        vec3 normal = normalize(frag_normal_world);
         ret = vec4(normal, 1.0);
     } else if (mode == 3) {
         float d = depth;
         ret = vec4(d, d, d, 1.0);
-    } else if (mode == 0) {
-        ret = vec4(1.0,0.5,0.5,1.0);
+    } else if (mode == 4) {
+        ret = vec4(frag_pos_cam, 1.0);
+    } else if (mode == 5) {
+        vec3 normal = normalize(frag_normal_cam);
+        ret = vec4(normal, 1.0);
     }
     outputF = ret;
 }
--- Shaders/gbuffer.vs.glsl
+++ Shaders/gbuffer.vs.glsl
@@ -9,16 +9,22 @@ uniform mat3 m_normalModel;
 in vec4 position;
 in vec3 normal;
 
-out vec3 frag_pos;      // frag_pos is the position of a pixel in world coordinate.
-out vec3 N;
+out vec3 frag_pos_world;      // frag_pos is the position of a pixel in world coordinate.
+out vec3 frag_pos_cam;       // position in view space;
+out vec3 frag_normal_world;
+out vec3 frag_normal_cam;
 out float depth;
 
 void main()
 {
     gl_Position = m_pvm * position;
+
     vec4 frag_pos_4 = m_model * position;
-    frag_pos = frag_pos_4.xyz;
-    N = normalize(m_normalModel * normal);
+    frag_pos_world = frag_pos_4.xyz;
+    frag_normal_world = normalize(m_normalModel * normal);
+
     vec4 c_pos = m_viewModel * position;
     depth = -c_pos.z;
+    frag_pos_cam = c_pos.xyz;
+    frag_normal_cam = normalize(m_normal * normal);
 } 
